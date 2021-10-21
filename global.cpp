414f672a4ebae90dcba5a2931fc41b0dfdc1223f
Ubuntu <ubuntu@ip-172-31-7-96.ec2.internal>
2021-10-21 16:21:24 +0000

    some commit message

---
+++ global.cpp
@@ -0,0 +1,33 @@
+// copyright (c) 2020 Joshua Yeung All rights reserved
+
+// Created by: Joshua Yeung
+// Created on September 2021
+// This program shows how local and global variables work
+
+#include <iostream>
+
+// global variable
+int variableX = 25;
+
+void localVariable() {
+    // this shows what happens with local variables
+    int variableX = 10;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableZ: "
+    << variableX << "+" << variableY << "=" << variableZ << std::endl;
+}
+
+void globalVariable() {
+    // this shows what happens with global variables
+    variableX = variableX + 1;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Global variableX, variableY, variableZ: "
+    << variableX << "+" << variableY << "=" << variableZ << std::endl;
+}
+int main() {
+    // this function calls local and global
+    localVariable();
+    globalVariable();
+}
